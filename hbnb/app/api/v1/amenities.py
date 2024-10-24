e1a1701ae62021096d637d31a5f8ae2c078b5071
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-10-24 19:32:10 -0400

    namespace added to init.py

--- hbnb/app/api/v1/amenities.py
+++ hbnb/app/api/v1/amenities.py
@@ -3,40 +3,38 @@
 from flask_restx import Namespace, Resource, fields
 from app.services.facade import HBnBFacade
 
-api = Namespace('amenities', description='Amenity Operations')
+api = Namespace('amenities', description='Amenity operations')
 
 amenity_model = api.model('Amenity', {
-    'name': fields.String(required=True, description='Name of the Amenity')
+    'name': fields.String(required=True, description='Name of the amenity')
 })
 
 facade = HBnBFacade()
 
+
 @api.route('/')
-class AmenityList(Resource)
-    api.expect(amenity_model)
-    api.response(201, 'Amenity successfully created')
-    api.response(400, 'Invalid input data')
+class AmenityList(Resource):
+    @api.expect(amenity_model)
+    @api.response(201, 'Amenity successfully created')
+    @api.response(400, 'Invalid input data')
     def post(self):
-        '''Register Amenity'''
         pass
 
     @api.response(200, 'List of amenities retrieved successfully')
     def get(self):
-        '''Retrieve a list of all amenities'''
         pass
 
-api.route('/<amenity_id>')
+
+@api.route('/<string:amenity_id>')
 class AmenityResource(Resource):
     @api.response(200, 'Amenity details retrieved successfully')
     @api.response(404, 'Amenity not found')
     def get(self, amenity_id):
-        '''Get amenity by id'''
         pass
 
-    api.expect(amenity_model)
-    api.response(200, 'Amenity updated successfully')
-    api.response(404, 'Amenity not found')
-    api.response(400, 'Invalid input data')
+    @api.expect(amenity_model)
+    @api.response(200, 'Amenity updated successfully')
+    @api.response(404, 'Amenity not found')
+    @api.response(400, 'Invalid input data')
     def put(self, amenity_id):
-        '''Updated an amenity's info'''
         pass
--- part2/app/models/amenity.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-
-from . import BaseModel
-
-
-class Amenity(BaseModel):
-    def __init__(self, name):
-        self.name = name
