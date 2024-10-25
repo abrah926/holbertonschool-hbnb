a03bc8ac8b64a08de946c82c40fe16d7bf35fbce
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-10-25 11:03:51 -0400

    updating logic for create place

--- hbnb/app/services/facade.py
+++ hbnb/app/services/facade.py
@@ -4,6 +4,7 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
 from app.models.amenity import Amenity
+from app.models.place import Place
 
 
 class HBnBFacade:
@@ -64,12 +65,44 @@ class HBnBFacade:
         return amenity
 
     def create_place(self, place_data):
-        # Placeholder for logic to create a place, including validation for price, latitude, and longitude
-        pass
+        try:
+            title = place_data.get('tittle')
+            price = place_data.get('price')
+            latitude = place_data.get('latitude')
+            longitude = place_data.get('longitude')
+            owner_id = place_data.get('owner_id')
+            amenities = place_data.get('amenities', [])
+
+            if title is None or price is None or latitude is None or longitude is None or owner_id is None:
+                raise ValueError('Missing fields requiered')
+
+            if price < 0:
+                raise ValueError('Price must be non-negative')
+            if not -90 <= latitude <= 90:
+                raise ValueError('Latitude must be between -90 and 90')
+            if not -180 <= longitude <= 180:
+                raise ValueError("Longitude must be between -180 and 180")
+
+            place = Place(
+                title=title,
+                description=place_data.get('description', ''),
+                price=price,
+                latitude=latitude,
+                longitude=longitude,
+                owner_id=owner_id,
+                amenities=amenities
+            )
+            return place
+        except ValueError as e:
+            raise ValueError(str(e))
 
     def get_place(self, place_id):
-        # Placeholder for logic to retrieve a place by ID, including associated owner and amenities
-        pass
+        '''Retrieves a place by ID, including owner and amenity'''
+        place = Place.get_by_id(place_id)
+        if place:
+            return place
+        else:
+            raise ValueError('place not found')
 
     def get_all_places(self):
         # Placeholder for logic to retrieve all places
