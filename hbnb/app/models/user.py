e9a65d9e56d37bb415abcb89d3b496008dd7c1a8
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-11-18 21:39:50 -0400

    updating user model to accept password hashing and adding db logic

--- hbnb/app/models/user.py
+++ hbnb/app/models/user.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-
-
+from werkzeug.security import generate_password_hash, check_password_hash
+from app.extensions import db
 from . import BaseModel
 
 
 class User(BaseModel):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(128), nullable=False)
+    last_name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
     def __init__(self, first_name, last_name, email, is_admin=False):
         super().__init__()
         self.first_name = first_name
@@ -12,3 +21,9 @@ class User(BaseModel):
         self.email = email
         self.is_admin = is_admin
         self.places = []
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
